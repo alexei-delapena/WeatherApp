f175038cf1bf5f40b9d812f2f57c304b4899852b
bizzaro <albert.fung@uoit.net>
2017-12-15 13:16:49 -0500

    pushing current code

---
+++ app/src/main/java/com/project/afinal/weatherapp/MapView.java
@@ -0,0 +1,302 @@
+package com.project.afinal.weatherapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class MapView extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private String OWM_TILE_URL = "http://tile.openweathermap.org/map/%s/%d/%d/%d.png?appid=e243c640522e46b7a9e8936150c5c489";
+    private String tileType = "";
+    private String[] tileTypesArray;
+    private TileOverlay tileOverlay;
+    private Spinner spinner;
+    private static String Location, CurrentTemp, Condition;
+    private String weatherAPI_url;
+    private LatLng newLatLng;
+    private ImageView legendImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        spinner = findViewById(R.id.tile_options);
+        tileTypesArray = new String[]{"Clouds", "Precipitation", "Pressure", "Wind", "Temperature"};
+
+        legendImage = findViewById(R.id.legend);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MapView.this, android.R.layout.simple_spinner_dropdown_item, tileTypesArray);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                switch (position){
+                    case 0:
+                        tileType = "clouds_new";
+                        tileOverlay.remove();
+                        Bitmap clouds = BitmapFactory.decodeResource(MapView.this.getResources(), R.raw.cloudbar);
+                        legendImage.setImageBitmap(clouds);
+                        initMap();
+                        break;
+                    case 1:
+                        tileType = "precipitation_new";
+                        tileOverlay.remove();
+                        Bitmap snow = BitmapFactory.decodeResource(MapView.this.getResources(), R.raw.snowbar);
+                        legendImage.setImageBitmap(snow);
+                        initMap();
+                        break;
+                    case 2:
+                        tileType = "pressure_new";
+                        tileOverlay.remove();
+                        Bitmap pressure = BitmapFactory.decodeResource(MapView.this.getResources(), R.raw.pressurebar);
+                        legendImage.setImageBitmap(pressure);
+                        initMap();
+                        break;
+                    case 3:
+                        tileType = "wind_new";
+                        tileOverlay.remove();
+                        Bitmap wind = BitmapFactory.decodeResource(MapView.this.getResources(), R.raw.windbar);
+                        legendImage.setImageBitmap(wind);
+                        initMap();
+                        break;
+                    case 4:
+                        tileType = "temp_new";
+                        tileOverlay.remove();
+                        Bitmap temp = BitmapFactory.decodeResource(MapView.this.getResources(), R.raw.temperaturebar);
+                        legendImage.setImageBitmap(temp);
+                        initMap();
+                        break;
+                }//end switch
+
+            }//end onItemSelected()
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }//end onNothingSelected()
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_today:
+                        Intent intent_today = new Intent(MapView.this, WeatherActivity.class);
+                        startActivity(intent_today);
+                        finish();
+                        break;
+                    case R.id.action_map:
+                        break;
+                    case R.id.action_Forecast:
+                        Intent intent_forecast = new Intent(MapView.this, ForecastView.class);
+                        startActivity(intent_forecast);
+                        finish();
+                        break;
+                }
+                return false;
+            }
+        });
+
+//        //Initialize the Map
+        initMap();
+
+    }//end onCreate()
+
+    //Initialize the Map
+    private void initMap() {
+        //Obtain support map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
+        supportMapFragment.getMapAsync(this);
+    }//end initMap()
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+//        groundOverlay(bitmapDescriptor);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MapView.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION}, 456);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //OverLay the Map with Weather Conditions
+        final TileProvider tileProvider = new UrlTileProvider(256, 256) {
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {
+//                String format_url = String.format(OWM_TILE_URL, tileType == null ? "clouds_new" : tileType, zoom, x, y);
+                String format_url = String.format(OWM_TILE_URL, tileType, zoom, x, y);
+                URL url = null;
+                try {
+                    url = new URL(format_url);
+                } catch (MalformedURLException ex) {
+                    ex.printStackTrace();
+                }
+                return url;
+            }
+        };
+        tileOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+
+        //OnMapClick
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+
+                newLatLng = latLng;
+                weatherAPI_url = "http://api.wunderground.com/api/2b118671ed3cb3c2/conditions/q/"+newLatLng.latitude+","+newLatLng.longitude+".json";
+                FetchWeatherData weatherFor_SelectedLocation = new FetchWeatherData();
+                weatherFor_SelectedLocation.execute(weatherAPI_url);
+
+                //Tile OverLay from Weather Imagery
+                tileOverlay = mMap.addTileOverlay(new TileOverlayOptions()
+                        .tileProvider(tileProvider));
+            }
+        });//end onMapClickListener
+
+    }//end onMapReady()
+
+/** ------------------ AsynTask Class ---------------- **/
+    private class FetchWeatherData extends AsyncTask<String, Void, String> {
+        String jsonData = null;
+        JSONObject weatherData;
+        StringBuilder sb = new StringBuilder();
+        String location="", currentTemp="", conditionType="";
+
+        @Override
+        protected String doInBackground(String... params) {
+            /* Parsing JSON DATA
+                -> JSON Data ULR ---->
+            */
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    if(!line.trim().isEmpty()){
+                        sb.append(line + "\n");
+                    }
+                }//end while loop
+
+                jsonData = sb.toString();
+                inputStream.close();
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonData;
+        }//end doInBackground()
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                weatherData = new JSONObject(jsonData);
+                JSONObject currentObservation_JSONObject = weatherData.getJSONObject("current_observation");
+                JSONObject displayLocation_JSONObject = currentObservation_JSONObject.getJSONObject("display_location");
+
+                location = displayLocation_JSONObject.getString("full");
+                currentTemp =  currentObservation_JSONObject.getString("temp_c");
+                conditionType = currentObservation_JSONObject.getString("weather");
+
+                Location = location;
+                CurrentTemp = currentTemp;
+                Condition = conditionType;
+
+                //Set Marker Options
+                MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(newLatLng.latitude, newLatLng.longitude))
+                        .title(Location)
+                        .snippet("Temperature: " + CurrentTemp + "\n" + "Current Conditions " + Condition +"\n")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                    @Override
+                    public View getInfoWindow(Marker marker) {
+                        return null;
+                    }
+                    @Override
+                    public View getInfoContents(Marker marker) {
+                        LinearLayout info = new LinearLayout(MapView.this);
+                        info.setOrientation(LinearLayout.VERTICAL);
+                        TextView title = new TextView(MapView.this);
+                        title.setTextColor(Color.BLACK);
+                        title.setGravity(Gravity.CENTER);
+                        title.setTypeface(null, Typeface.BOLD);
+                        title.setText(marker.getTitle());
+
+                        TextView snippet = new TextView(MapView.this);
+                        snippet.setTextColor(Color.GRAY);
+                        snippet.setText(marker.getSnippet());
+                        info.addView(title);
+                        info.addView(snippet);
+                        return info;
+                    }
+                });
+                mMap.addMarker(markerOptions).showInfoWindow(); //Set the Marker
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(newLatLng.latitude, newLatLng.longitude), 5f)); //Move the Camera to the Marker
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }//end onPostExecute()
+    }//end FetchWeatherData.class
+}//end MapView.class
---
+++ app/src/main/java/com/project/afinal/weatherapp/WeatherActivity.java
@@ -0,0 +1,230 @@
+package com.project.afinal.weatherapp;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ * This Activity Displays TODAY'S Weather -> The Weather is updated every 2.5 hours
+ * Using "Open Weather Map API"
+ */
+public class WeatherActivity extends AppCompatActivity {
+
+    private static final String LogT = "WEATHER_ACTIVITY :: ";
+
+    //Declaring Variables
+    DeviceLocation deviceLoc;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    String wunderground_API;
+    String weatherAPI_url;
+    FetchWeatherData weatherData;
+    TextView lbl_currentTemp, lbl_feelsLike, lbl_windSpeed, lbl_location, lbl_conditionsType, lbl_humitidy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        // --- TextView ---
+        lbl_currentTemp = findViewById(R.id.CurrentTemp);
+        lbl_feelsLike = findViewById(R.id.FeelsLike);
+        lbl_windSpeed = findViewById(R.id.WindSpeed);
+        lbl_location = findViewById(R.id.Location);
+        lbl_conditionsType = findViewById(R.id.Condition_type);
+        lbl_humitidy = findViewById(R.id.humidity);
+
+        //Initializing Variables
+        wunderground_API = getResources().getString(R.string.wunderground_API);
+//        weatherAPI_url = "http://api.wunderground.com/api/"+ wunderground_API+"/conditions/q/43.6532,-79.3832.json"; //Location -> Downtown Toronto
+//        weatherData = new FetchWeatherData(); //Download JSON data using the 'wunderground' API
+//        weatherData.execute(weatherAPI_url);
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            //Alert the User if GPS is Not Enabled
+            AlertDialog.Builder builder = new AlertDialog.Builder(WeatherActivity.this);
+            builder.setTitle("Please Enable GPS");
+            builder.setMessage("Location Services must be enabled to receive weather data for your current location");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent gpsOptionsIntent = new Intent(
+                            android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(gpsOptionsIntent);
+                }
+            }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    weatherAPI_url = "http://api.wunderground.com/api/"+ wunderground_API+"/conditions/q/43.6532,-79.3832.json"; //Location -> Downtown Toronto
+                    weatherData = new FetchWeatherData(); //Download JSON data using the 'wunderground' API
+                    weatherData.execute(weatherAPI_url);
+                }
+            }).setIcon(android.R.drawable.ic_dialog_alert).show();
+        }//end if block --> Check if location is enabled
+        else{
+            getDeviceLocation();
+        }
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_today:
+                        break;
+                    case R.id.action_map:
+                        Intent intent_map = new Intent(WeatherActivity.this, MapView.class);
+                        startActivity(intent_map);
+                        break;
+                    case R.id.action_Forecast:
+                        Intent intent_forecast = new Intent(WeatherActivity.this, ForecastView.class);
+                        startActivity(intent_forecast);
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }//end onCreate()
+
+    //Button Click --> Fetch Toronto Data
+    public void getWeatherData(View view){
+//        weatherData = new FetchWeatherData(); //Download JSON data using the 'wunderground' API
+//        weatherData.execute(weatherAPI_url);
+        getDeviceLocation();
+    }//end getWeatherData()
+
+
+    // ---------------------- Get Device Location ---------------------- //
+    public void getDeviceLocation(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.INTERNET}, 231);
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Log.d(LogT, "Location: " + location);
+                onLocationChange(location);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(LogT, "Error while getting Device Location");
+            }
+        });
+    }//end getDeviceLocation()
+
+    public void onLocationChange(Location loc){
+        Log.d(LogT, "Location: " + loc);
+        weatherAPI_url = "http://api.wunderground.com/api/" + wunderground_API + "/conditions/q/"+loc.getLatitude()+","+loc.getLongitude()+".json";
+        weatherData = new FetchWeatherData(); //Download JSON data using the 'wunderground' API
+        weatherData.execute(weatherAPI_url);
+    }//end onLocationChange()
+
+
+    /** ------------------ AsynTask Class ---------------- **/
+    private class FetchWeatherData extends AsyncTask<String, Void, String> {
+        String jsonData = null;
+        JSONObject weatherData;
+        StringBuilder sb = new StringBuilder();
+        String currentTemp = "";
+        String feelsLike="", windSpeed="", location="", conditionType="", humidity="";
+
+        @Override
+        protected String doInBackground(String... params) {
+            /* Parsing JSON DATA */
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    if(!line.trim().isEmpty()){
+                        sb.append(line + "\n");
+                    }
+                }//end while loop
+
+                jsonData = sb.toString();
+                inputStream.close();
+                bufferedReader.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonData;
+        }//end doInBackground()
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                weatherData = new JSONObject(jsonData);
+                JSONObject currentObservation_JSONObject = weatherData.getJSONObject("current_observation");
+                JSONObject displayLocation_JSONObject = currentObservation_JSONObject.getJSONObject("display_location");
+
+                location = displayLocation_JSONObject.getString("full");
+                currentTemp =  currentObservation_JSONObject.getString("temp_c");
+                feelsLike = currentObservation_JSONObject.getString("feelslike_c");
+                windSpeed = currentObservation_JSONObject.getString("wind_kph");
+                conditionType = currentObservation_JSONObject.getString("weather");
+                humidity = currentObservation_JSONObject.getString("relative_humidity");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //Set TextViews
+            lbl_currentTemp.setText(Math.round(Double.parseDouble(currentTemp)) + " °C"); //'°' --> ALT 248
+            lbl_feelsLike.setText(feelsLike + " °C");
+            lbl_windSpeed.setText(windSpeed + " K/H");
+            lbl_location.setText(location);
+            lbl_conditionsType.setText(conditionType);
+            lbl_humitidy.setText(humidity);
+        }//end onPostExecute()
+    }//end FetchWeatherData.class
+
+}//end WeatherActivity.class
